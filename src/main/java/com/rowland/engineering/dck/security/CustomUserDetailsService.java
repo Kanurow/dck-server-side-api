cee7bfe597256e517f39d76d29c56652fc52642a
Kanu Rowland <kanurowland92@gmail.com>
2024-07-23 00:58:23 +0100

    cleaned user details

--- src/main/java/com/rowland/engineering/dck/security/CustomUserDetailsService.java
+++ src/main/java/com/rowland/engineering/dck/security/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import com.rowland.engineering.dck.model.User;
 import com.rowland.engineering.dck.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,11 +14,14 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     @Transactional
